7786d5635b095fa85fba33f78925fbb38d3cb76f
Michael Pay <wanderlust.wayfarer@icloud.com>
2023-11-08 14:14:13 -0800

    set_configs zshrc

--- set_configs.sh
+++ set_configs.sh
@@ -2,13 +2,30 @@
 
 # Description: Overrides default configs. Run this if you have updates to your working environment and commit to your fork.
 
-# Homebrew configs
-echo "Collecting Homebrew formulae & casks..."
-brew list --formulae > ./Configs/brew_casks_and_formulae.txt
-brew list --casks >> ./Configs/brew_casks_and_formulae.txt
-echo "Done"
+# FUNCTIONS
+# Collect Homebrew formulae & casks
+function set_homebrew_casks_and_formulae () {
+    echo "Collecting Homebrew formulae & casks..."
+    brew list --formulae > ./Configs/brew_casks_and_formulae.txt
+    brew list --casks >> ./Configs/brew_casks_and_formulae.txt
+    echo "Done"
+}
 
-# VS Code extensions
-echo "Setting VS Code extensions"
-code --list-extensions > ./Configs/vscode_extensions.txt
-echo "Done"
+# Collect VS Code extensions
+function set_vscode_extensions () {
+    echo "Setting VS Code extensions"
+    code --list-extensions > ./Configs/vscode_extensions.txt
+    echo "Done"
+}
+
+# Sets all Shell preferences
+function set_shell_preferences () {
+    copy_zshrc
+}
+
+# Copy .zsrhc
+function copy_zshrc () {
+    echo "Copying .zshrc from Home (~) directory"
+    cp ~/.zshrc ./Configs
+    echo "Done"
+}
